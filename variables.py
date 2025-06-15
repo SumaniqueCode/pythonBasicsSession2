2c0d6bf8de1307e058242c7f4559ba71ea438454
Suman Regmi <limitlessplaying@gmail.com>
2025-06-15 16:42:34 +0545

    variables in python

---
+++ variables.py
@@ -0,0 +1,13 @@
+string1 = "This is basics of python"
+string2 = " This is pathshala"
+
+# This is adding strings
+print(string1 + string2)
+
+num1 = 1
+num2 = 20
+
+# adding integers
+sum = num1 + num2
+print("This is sum", sum)
+print(f"This is sum {sum}")
