b0a73b5a20f567ef281977bc0b33c091c6d500c9
Suman Regmi <limitlessplaying@gmail.com>
2025-06-15 16:43:03 +0545

    Operators in python

---
+++ operators.py
@@ -0,0 +1,35 @@
+#operators in python
+#1. Arithmetic operators +, -, *, /, %, **, //
+
+num1 = 10
+num2 = 15
+
+sum = num1+num2
+print("Sum of two numbers is:", sum)
+
+rem = num2 % num1
+print(rem)
+
+pow = num1**2
+print(pow)
+
+div = num2//2
+print(div)
+
+#2. Comparison operators ==, !=, >, <, >=, <=
+print(num1>num2)
+print(num1!=num2)
+print(num1==num2)
+print(num1<=num2)
+
+#3 logical operators and, or, not
+#and operator
+print(num1>=num2 and num2<=num1)
+print(num1<=num2 and num2>=num1)
+
+#or operator
+print(num1>=num2 or num2<=num1)
+print(num1<=num2 or num2<=num1)
+print (not num1>num2)
+
+
