b0602c7cc19f6f54eb8edeb0a71ec16ed54b6a36
Suman Regmi <limitlessplaying@gmail.com>
2025-06-15 16:42:17 +0545

    Taking data from the users

---
+++ scanning_data.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+print(f"Hello {name} !")
+
+num1 = int(input("Enter any number: "))
+num2 = int(input("Enter any number: "))
+print(f"The sum of {num1} and {num2} is {num1+num2}")
