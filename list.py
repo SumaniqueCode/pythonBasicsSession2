152f0349ac058b0e2356676954be421783ef94c3
Suman Regmi <limitlessplaying@gmail.com>
2025-06-16 16:58:57 +0545

    List and functions

---
+++ list.py
@@ -0,0 +1,26 @@
+# list in python
+# list are mutable , i.e., they can be modified after creation.
+
+list1 = ["Hello", "World", "Python", "Programming", 1, 2.4, True, None, "Hello", 2.4]
+print(list1) #printing whole list
+print(list1[1]) #printing through index
+print(list1[2:5]) #printing through index range
+print(list1[ :5]) #printing through index range from start to 5
+print(list1[5: ]) #printing from index 5 to end
+list1.append('Appended string') #appending element at the end
+print(list1)
+print(len(list1))
+list1.reverse()  # reversing the list
+print(list1)  # printing reversed list
+list1.remove(2.4)
+print(list1)
+list1.pop()
+print(list1)
+
+#Printing using len function
+for i in range (len(list1)):
+    print(list1[i], end= " ")
+
+#printing every items in list using for loop
+for item in list1:
+    print(item, end=" ")
