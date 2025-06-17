c32d5a64b20db29b762a970dea51aa6ea022ca0b
Suman Regmi <limitlessplaying@gmail.com>
2025-06-17 16:19:40 +0545

    Nested and multidimensional list

---
+++ multidimesional_nested_list.py
@@ -0,0 +1,11 @@
+# multidimensional list
+list1 = [2, 5, 10, 12], [3, 7, 11, 13]
+# print(list1[0])  # prints the first sublist i.e of index 0
+# print(list1[0][2])  # prints the third element of the first sublist i.e of index 0 and 2
+# print(list1[0] + list1[1])  # add the two sublists together
+
+#nested list
+list2 = [9, 65, [44,62,55, 72, 1], 3, 7, 8]
+print(list2[2]) # print the third element of the list i.e of index 2
+print(list2[2][2]) # printing the third element of the sublist i.e of index 2 and 2
+
