b1f0644f93f73f3807bc2c49677caa1c9b36994c
Suman Regmi <limitlessplaying@gmail.com>
2025-06-17 16:09:07 +0545

    List functions

--- list.py
+++ list.py
@@ -2,6 +2,7 @@
 # list are mutable , i.e., they can be modified after creation.
 
 list1 = ["Hello", "World", "Python", "Programming", 1, 2.4, True, None, "Hello", 2.4]
+list2= [5,8,11,20,34,49,1]
 print(list1) #printing whole list
 print(list1[1]) #printing through index
 print(list1[2:5]) #printing through index range
@@ -16,8 +17,21 @@ list1.remove(2.4)
 print(list1)
 list1.pop()
 print(list1)
+list2.sort()
+print("Sorted list: ", list2)  # sorting the list in ascending order
 
-#Printing using len function
+list2.sort(reverse=True)
+print("Sorted list: ", list2)  # sorting the list in descending order
+
+list2.insert(2, 10) # inserting element at index 2
+print(list2)
+
+#for clearing list
+list2.clear()
+print(list2)
+
+
+# Printing using len function
 for i in range (len(list1)):
     print(list1[i], end= " ")
 
