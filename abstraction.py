38c19e31af88b63c46f47f2ef936982dc72c9e7f
Suman Regmi <limitlessplaying@gmail.com>
2025-06-20 15:08:25 +0545

    Abstraction

--- abstraction.py
+++ abstraction.py
@@ -3,23 +3,9 @@
 from abc import ABC, abstractmethod
 
 class Wallets(ABC):
-    @abstractmethod
-    def pay(self):
+    def pay(self,amount):
         pass
 
 class Paypal(Wallets):
-    def pay(self):
+    def pay(self,):
         print("Paypal payment successful")
-
-class MasterCard(Wallets):
-    def pay(self):
-        print("MasterCard payment successful")
-
-#displaying the abstract class
-def displayData( wallet : Wallets, amount):
-    print("This is a function to display data")
-    wallet.pay()
-    print("Amount to be paid is: ", amount)
-
-displayData( Paypal(), 1000)
-
